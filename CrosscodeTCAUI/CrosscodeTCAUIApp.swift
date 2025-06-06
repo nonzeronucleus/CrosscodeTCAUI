2247d6d7f9b36943c6effdc50ebb863c8e70d4cf
Ian <nonzeronucleus@ianplumb.org>
2025-06-06 14:27:08 +0100

    Initial Commit

---
+++ CrosscodeTCAUI/CrosscodeTCAUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrosscodeTCAUIApp.swift
+//  CrosscodeTCAUI
+//
+//  Created by Ian Plumb on 06/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct CrosscodeTCAUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CrosscodeTCAUITests/CrosscodeTCAUITests.swift
@@ -0,0 +1,17 @@
+//
+//  CrosscodeTCAUITests.swift
+//  CrosscodeTCAUITests
+//
+//  Created by Ian Plumb on 06/06/2025.
+//
+
+import Testing
+@testable import CrosscodeTCAUI
+
+struct CrosscodeTCAUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
